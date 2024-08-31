57e32d8d212c0b0d7f2ac4b19ee13a5f448f7204
Leviathan <soaresjsantos93@gmail.com>
2024-08-31 00:36:39 -0300

    Feat: prime counting functions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "sieve_eratosthenes.hpp"
+#include "counting_primes.hpp"
 #include <iostream>
 #include <cmath>
 
@@ -9,7 +10,26 @@ int main()
 
     std::pair<long long, long long> primes = sieve_eratosthenes(n);
 
+    std::cout << "π(n): " << primes.first + primes.second + 1 << '\n';
 
-    std::cout << "Pi function: " << 
-    std::cout << primes.first << ' ' << primes.second << '\n';
+    double s = s_function(n), pnt = prime_number_theory(n);
+    std::cout << "S(n): " << s << '\n';
+    std::cout << "Prime number theory (PNT) (n): " << pnt << '\n';
+
+    std::cout << '\n';
+
+    std::cout << "π(n) - S(n): " << (primes.first + primes.second + 1) - s << '\n';
+    std::cout << "π(n) - PNT(n): "<< (primes.first + primes.second + 1) - pnt << '\n';
+
+    std::cout << '\n';
+
+    std::cout << "π1: p = 1 mod 4, π3: p = 3 mod 4\n";
+    std::cout << "π1(n): " << primes.first << '\n';
+    std::cout << "π3(n): " << primes.second << '\n';
+
+    std::cout << "π1(n) / π3(n): " << primes.first / (double) primes.second << '\n';
+
+
+
+
 }
